01bd9894a068deb781cdd53414dcdffe72ccfc8c
Martin Håberg <martin.haberg@ntnu.no>
2017-03-27 13:31:39 +0200

    Built model from scratch

---
+++ demo2.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 27 13:22:11 2017
+
+@author: martihab
+"""
+
+from gurobipy import *
+
+m = Model()
+
+x = m.addVar(vtype=GRB.BINARY, name="x")
+y = m.addVar(vtype=GRB.BINARY, name="y")
+z = m.addVar(vtype=GRB.BINARY, name="z")
+
+m.setObjective(x + y + 2*z, GRB.MAXIMIZE)
+
+c1 = m.addConstr(x + 2*y + 3*z <= 4)
+c2 = m.addConstr(x + y >= 1)
+
+m.optimize()
+
+m.printAttr('X')
